dc129db1fdfc79e5aeaea5352497d8b82dc5fee5
zxcvin <zxcvin98@gmail.com>
2020-09-01 09:38:25 -0700

    Completed Challenge

--- appbrewery_flutter/magic-8-ball-flutter-master/lib/main.dart
+++ appbrewery_flutter/magic-8-ball-flutter-master/lib/main.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      AppPage(),
+    );
+
+class AppPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[700],
+        appBar: AppBar(
+          title: Text(
+            'Ask Me Anything',
+            style: TextStyle(
+              fontSize: 35.0,
+            ),
+          ),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: BallPage(),
       ),
     );
+  }
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
